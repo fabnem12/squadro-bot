3b947fe4d1c6b9948818d706237a9547c66ad900
fabnem12 <48524864+fabnem12@users.noreply.github.com>
2021-01-25 10:16:33 +0100

    Petite amélioration de la sécurité

--- pythonBot/pythonbot.py
+++ pythonBot/pythonbot.py
@@ -21,7 +21,7 @@ DUREE_EXEC = 5
 DUREE_EXEC_ADMIN = 20
 DUREE_EXEC_RAB = 60
 
-commandesInterdites = ["input(", "open(", "= open"]
+commandesInterdites = ["input(", "open(", "= open", "=open"]
 importsAutorises = {"cmath", "math", "matplotlib", "numpy", "pandas", "random", "SquadroBot", "time", "scipy", "pythonBot"}
 substituts = {"%ptrlib": "from SquadroBot import ptrlib\nglobals().update(ptrlib())", "%llistlib": "from SquadroBot import linkedlistlib\nglobals().update(linkedlistlib())", "plt.show()": "getPlot()"}
 substituts.update({"%matplotlib inline": ""})
