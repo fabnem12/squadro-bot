304cbba19faf0fb58de9a079ed9273d0c679dfc4
fabnem12 <48524864+fabnem12@users.noreply.github.com>
2021-01-30 15:29:35 +0100

    Update votenew.py

--- pythonBot/votenew.py
+++ pythonBot/votenew.py
@@ -50,10 +50,6 @@ def main():
         channel = await dmChannelUser(user)
 
         if message.id in MSG2VOTE: #on est là après avoir cliqué sur la réaction pour voter
-            if userId not in {619574125622722560, 476163854858977309, 495283249493442570, 257924378795180032, 356510267833712645, 646480116884570162, 690496497476960256, 235694387248627712, 519205599548801034}:
-                await channel.send("Tu n'as le droit de voter que si t'es dans le groupe 9B…")
-                return
-
             election = MSG2VOTE[message.id]
             if election.fini():
                 await channel.send("Désolé, le dépouillement a déjà eu lieu")
