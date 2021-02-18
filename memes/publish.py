30ecd7092ac408750fdeef05bb6b3eac8498d345
sykzen <sykzen@hotmail.com>
2021-02-18 11:01:42 +0100

    implement: requirements, modify:readme,folder memes

---
+++ memes/publish.py
@@ -0,0 +1,27 @@
+import memes
+import managedata
+import time
+import discord
+#----------------------------------------------------------------------------------------
+
+NumberOfMemesCsPerDay=2 "le nombre de memes computer science à publier par jour"
+NumberOfMemesPerDay=1   "le nombre de memes normal à publier par jour "
+totalMemes=NumberOfMemesPerDay+NumberOfMemesCsPerDay
+
+#ne pas mettre des heures similaires
+TimeToPublishMemesCS=[] "Liste des heures où publier les memesCS heure france (integers 0<h<24)"
+TimeToPublishMemes=[] "Liste des heures où publier les memes heure france (integers 0<h<24)"
+
+def publish():
+    """fonction pour publier chaque jours le quota de memes demandé"""
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
