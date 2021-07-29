2efc3b854764ef739b2e32faa81dff24b08b3495
fabnem <fabnem12@gmail.com>
2021-07-29 21:14:38 +0200

    améliorations

--- maj.py
+++ maj.py
@@ -4,19 +4,22 @@ import pickle
 import psutil
 import sys
 
+def killBot():
+    if "fichierPID.p" in os.listdir():
+        pidsBot = pickle.load(open("fichierPID.p", "rb"))
+
+        for pid in pidsBot:
+            if psutil.pid_exists(pid):
+                psutil.Process(pid).terminate()
+
 listeScripts = ["pythonBot/pythonbot.py", "pythonBot/votenew.py", "discordUtils/ranks.py", "discordUtils/ranks2.py", "squadroGame/bot.py", "discordUtils/discordutils.py", "squadroGame/botNew.py", "voltBot/bump_leaderboard.py"]
 
-if "fichierPID.p" in os.listdir():
-    pidsBot = pickle.load(open("fichierPID.p", "rb"))
-
-    for pid in pidsBot:
-        if psutil.pid_exists(pid):
-            psutil.Process(pid).terminate()
+killBot()
 
 pickle.dump(set(), open("fichierPID.p", "wb"))
 os.system("git pull")
 
-with open("logs_bot.log", "w"): pass #astuce pour effacer les logs au démarrage
+with open("logs_bot.log", "w") as f: f.write(" ") #astuce pour effacer les logs au démarrage
 
 foutput = open("logs_bot.log", "a")
 for path in listeScripts:
