d9817e248003a1cf32db34ee218885eb3dd135a7
fabnem <fabnem12@gmail.com>
2021-09-22 21:47:30 +0200

    logic lib suite

--- pythonBot/LogicLib.py
+++ pythonBot/LogicLib.py
@@ -73,16 +73,20 @@ class And(OpBin):
         return bool(self.a) and bool(self.b)
 
     def value(self, subst: Optional[Dict[Variable, bool]] = None):
-        valA, valB = self.a.value(subst), self.b.value(subst)
+        valA = self.a.value(subst)
 
-        if valA is True:
-            return valB
-        elif valA is False or valB is False:
+        if valA is False:
             return False
-        elif valB is True:
-            return valA
         else:
-            return valA & self.b.value(subst)
+            valB = self.b.value(subst)
+            if valA is True:
+                return valB
+            elif valB is False:
+                return False
+            elif valB is True:
+                return valA
+            else:
+                return valA & valB
 
 class Or(OpBin):
     def __str__(self):
@@ -92,17 +96,74 @@ class Or(OpBin):
         return bool(self.a) or bool(self.b)
 
     def value(self, subst: Optional[Dict[Variable, bool]] = None):
-        valA, valB = self.a.value(subst), self.b.value(subst)
+        valA = self.a.value(subst)
 
-        if valA is True or valB is True:
+        if valA is True:
             return True
-        elif valA is False:
-            return valB
-        elif valB is False:
-            return valA
         else:
-            return valA | valB
+            valB = self.b.value(subst)
 
+            if valB is True:
+                return True
+            elif valA is False:
+                return valB
+            elif valB is False:
+                return valA
+            else:
+                return valA | valB
 
-def vars(nbVars = 1, defaultVal = None):
-    return tuple(Variable(f"x{i+1}", defaultVal) for i in range(nbVars))
+class Impl(OpBin):
+    def __str__(self):
+        return f"({str(self.a)} => {str(self.b)})"
+
+    def __bool__(self):
+        return not bool(self.a) or bool(self.b)
+
+    def value(self, subst: Optional[Dict[Variable, bool]] = None):
+        valA = self.a.value(subst)
+
+        if valA is False:
+            return True
+        else:
+            valB = self.b.value(subst)
+
+            if valB is True:
+                return True
+            elif valA is True:
+                return valB
+            elif valB is False:
+                return False
+            else:
+                return Impl(valA, valB)
+
+def vars():
+    compteur = [0]
+    def aux(nbVars = 1, defaultVal = None):
+        ret = tuple(Variable(f"x{compteur[0]+i+1}", defaultVal) for i in range(nbVars))
+        compteur[0] += nbVars
+        return ret if len(ret) > 1 else ret[0]
+    return aux
+vars = vars()
+
+def varsOfExpr(expr: Expression):
+    if expr in (True, False):
+        return set()
+    elif isinstance(expr, Variable):
+        return {expr}
+    elif isinstance(expr, OpBin):
+        return varsOfExpr(expr.a) | varsOfExpr(expr.b)
+    elif isinstance(expr, Not):
+        return varsOfExpr(expr.expr)
+
+def tableVerite(expr: Expression):
+    variables = sorted(varsOfExpr(expr), key=lambda x: x.name)
+    longueurNomMax = max((len(x.name) for x in variables), default=0)
+    tab = " "
+
+    affi = f'{" ".join(map(str, variables))}{tab}|{tab}{str(expr)}\n'
+    affi += "-" * (len(affi) - 1) + "\n"
+    for n in range(1 << len(variables)): #on fait toutes les combinaisons booléennes possibles des variables
+        subst = {x: bool((n >> i) % 2) for i, x in enumerate(variables)}
+        affi += f'{tab.join("V" if subst[x] else "F" for x in variables)}{tab}|{tab}{"V" if bool(expr.value(subst)) else "F"}\n'
+
+    return affi
