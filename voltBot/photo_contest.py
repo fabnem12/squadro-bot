e9ab77287f20bdd0c4bcf664c35c941a2861ac54
fabnem <fabnem12@gmail.com>
2021-10-04 10:45:06 +0200

    fixes

--- voltBot/photo_contest.py
+++ voltBot/photo_contest.py
@@ -126,9 +126,6 @@ else:
     msg2submission = dict()
     save()
 
-SUPERFINAL = 765232858499252264
-prefix = "T."
-
 def countryRolesUser(user):
     listRoles = {'Vatican', 'Ukraine', 'United Kingdom', 'Turkey', 'Switzerland', 'Sweden', 'Spain', 'Slovenia', 'Slovakia', 'Serbia', 'San Marino', 'Portugal', 'Russia', 'Romania', 'Poland', 'Norway', 'North Macedonia', 'Netherlands', 'Montenegro', 'Monaco', 'Moldova', 'Malta', 'Luxembourg', 'Lithuania', 'Liechtenstein', 'Latvia', 'Kazakhstan', 'Kosovo', 'Italy', 'Ireland', 'Iceland', 'Hungary', 'Greece', 'Georgia', 'Germany', 'France', 'Finland', 'Estonia', 'Denmark', 'Czechia', 'Cyprus', 'Croatia', 'Bulgaria', 'Bosnia & Herzegovina', 'Belgium', 'Belarus', 'Azerbaijan', 'Austria', 'Andorra', 'Armenia', 'Albania'}
     return set(x.name for x in user.roles if x.name in listRoles)
@@ -347,13 +344,6 @@ def main() -> None:
     @bot.event
     async def on_ready():
         autoplanner.start()
-        
-        categ = CATEGORIES["food"]
-        for proposal in categ.proposals:
-            if "https://cdn.discordapp.com/attachments/847488864713048144/892855813775970346/https--cdn.discordapp.com-attachments-877626245582573668-892855801071411250-image0.jpg" in proposal.url:
-                categ.votes[proposal] = set()
-
-        save()
 
     async def traitementRawReact(payload):
         if payload.user_id != bot.user.id: #sinon, on est dans le cas d'une réaction en dm
